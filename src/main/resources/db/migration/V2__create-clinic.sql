5d66cda0d0561b37b4c3448086cb4e6290ba1771
Eduardo Darwich <eduardo.darwich23@gmail.com>
2025-10-17 16:43:21 -0300

    Alteração no campo para "name" unique

--- src/main/resources/db/migration/V2__create-clinic.sql
+++ src/main/resources/db/migration/V2__create-clinic.sql
@@ -1,6 +1,6 @@
 create table clinic (
 id UUID primary key,
-name text not null,
+name text unique not null,
 cnpj CHAR(14) unique not null,
 address text not null,
 telephone VARCHAR(20) unique not null
