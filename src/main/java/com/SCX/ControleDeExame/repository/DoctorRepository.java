e147ee0f5269eb944de9be7133b84307426bae09
Eduardo Darwich <eduardo.darwich23@gmail.com>
2025-10-17 16:42:03 -0300

    Query para Consultar as clínicas que o médico está vinculado pelo id do médico

--- src/main/java/com/SCX/ControleDeExame/repository/DoctorRepository.java
+++ src/main/java/com/SCX/ControleDeExame/repository/DoctorRepository.java
@@ -1,12 +1,25 @@
 package com.SCX.ControleDeExame.repository;
 
+import com.SCX.ControleDeExame.dataTransferObject.doctorDTO.ResponseClinicMedDTO;
 import com.SCX.ControleDeExame.domain.doctor.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface DoctorRepository extends JpaRepository <Doctor, UUID> {
     Doctor findByAuthId_Id(UUID id);
     Doctor findByCrm(String crm);
+    boolean existsByCrm(String crm);
+
+    @Query("""
+            select new com.SCX.ControleDeExame.dataTransferObject.doctorDTO.ResponseClinicMedDTO(c.name)
+            from Clinic c
+            join c.doctors d
+            where d.id = :doctorId
+            """)
+    List<ResponseClinicMedDTO> findClinicByDoctor(@Param("doctorId") UUID doctorId);
 
 }
