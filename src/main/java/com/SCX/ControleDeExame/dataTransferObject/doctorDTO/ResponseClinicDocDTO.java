d6b9dd8f60189d8a804fb7df38f4d53955069cba
Eduardo Darwich <eduardo.darwich23@gmail.com>
2025-10-18 09:20:09 -0300

    Refactor

--- src/main/java/com/SCX/ControleDeExame/dataTransferObject/doctorDTO/ResponseClinicDocDTO.java
+++ src/main/java/com/SCX/ControleDeExame/dataTransferObject/doctorDTO/ResponseClinicDocDTO.java
@@ -2,8 +2,8 @@ package com.SCX.ControleDeExame.dataTransferObject.doctorDTO;
 
 import com.SCX.ControleDeExame.domain.clinic.Clinic;
 
-public record ResponseClinicMedDTO(String name) {
-    public ResponseClinicMedDTO(Clinic clinic){
+public record ResponseClinicDocDTO(String name) {
+    public ResponseClinicDocDTO(Clinic clinic){
         this(clinic.getName());
     }
 }
