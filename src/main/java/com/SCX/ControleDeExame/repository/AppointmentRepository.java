bf9b782d1c822f8caecc1b38445285e796794531
Eduardo Darwich <eduardo.darwich23@gmail.com>
2025-10-22 19:12:36 -0300

    Query de busca

--- src/main/java/com/SCX/ControleDeExame/repository/AppointmentRepository.java
+++ src/main/java/com/SCX/ControleDeExame/repository/AppointmentRepository.java
@@ -1,8 +1,19 @@
 package com.SCX.ControleDeExame.repository;
 
+import com.SCX.ControleDeExame.domain.appointment.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
-public interface AppointmentRepository extends JpaRepository<com.SCX.ControleDeExame.domain.appointment.Appointment, UUID> {
+public interface AppointmentRepository extends JpaRepository<Appointment, UUID> {
+
+    @Query("""
+            select a
+            from Appointment a
+            join a.doctor d
+            where d.id = :doctorId and a.openAppointment = true
+            """)
+    Appointment findByDoctorAvaiable(@Param("doctorId") UUID doctorId);
 }
