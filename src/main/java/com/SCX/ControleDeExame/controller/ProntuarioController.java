c822cfb4f5913d2a98fd883996a2f04af224576d
Eduardo Darwich <eduardo.darwich23@gmail.com>
2025-11-06 02:18:36 -0300

    Metodo de transformar a requisição de exame em pdf criado

--- src/main/java/com/SCX/ControleDeExame/controller/ProntuarioController.java
+++ src/main/java/com/SCX/ControleDeExame/controller/ProntuarioController.java
@@ -9,10 +9,7 @@ import com.SCX.ControleDeExame.service.DoctorService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/prontuario")
@@ -23,25 +20,25 @@ public class ProntuarioController {
 
     //Rota para devolver a anamnese da consulta referente ao atendimento do prontuario
     @GetMapping("/getAnamneseConsult")
-    public ResponseEntity<ResponseAnamnesisDTO> getAnamneseConsult (@RequestBody @Valid GetAppointmentIdDTO data){
+    public ResponseEntity<ResponseAnamnesisDTO> getAnamneseConsult (@RequestParam("id") GetAppointmentIdDTO data){
         return ResponseEntity.ok(doctorService.getAnamneseByConsult(data));
     }
 
     //Rota para retornar o diagnostico de uma consulta
     @GetMapping("/getDiagnostic")
-    public ResponseEntity<ReturnDiagnosticDTO> getDiagnostic (@RequestBody @Valid GetAppointmentIdDTO data){
+    public ResponseEntity<ReturnDiagnosticDTO> getDiagnostic (@RequestParam("id") GetAppointmentIdDTO data){
         return ResponseEntity.ok(doctorService.returnDiagnostic(data));
     }
 
     //Rota para retornar as requisições de exame de uma consulta
     @GetMapping("/getExamsRequest")
-    public ResponseEntity<ReturnExamsRequestsDTO> getExamRequest (@RequestBody @Valid GetAppointmentIdDTO data){
+    public ResponseEntity<ReturnExamsRequestsDTO> getExamRequest (@RequestParam("id") GetAppointmentIdDTO data){
         return ResponseEntity.ok(doctorService.returnExamsRequests(data));
     }
 
     //Rota para retornar os exames de uma consulta
     @GetMapping("/getExams")
-    public ResponseEntity<ReturnExamsResultsDTO> getExams (@RequestBody @Valid GetAppointmentIdDTO data){
+    public ResponseEntity<ReturnExamsResultsDTO> getExams (@RequestParam("id") GetAppointmentIdDTO data){
         return ResponseEntity.ok(doctorService.returnExamsResults(data));
     }
 
