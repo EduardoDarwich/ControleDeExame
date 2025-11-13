1f95bda16effb0dad8697152a545773c31add6f8
Eduardo Darwich <eduardo.darwich23@gmail.com>
2025-11-13 05:03:01 -0300

    Metodos do adm do sistema e bug fixes

--- src/main/java/com/SCX/ControleDeExame/infra/security/SecurityFilter.java
+++ src/main/java/com/SCX/ControleDeExame/infra/security/SecurityFilter.java
@@ -25,7 +25,7 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Autowired
     AuthRepository authRepository;
-
+    //teste
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var token = this.recoverToken(request);
