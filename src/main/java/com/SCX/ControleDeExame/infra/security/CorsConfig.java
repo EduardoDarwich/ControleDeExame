b85e47a9068337e169a1b340b1b7a6c87c139261
Eduardo Darwich <eduardo.darwich23@gmail.com>
2025-11-14 04:22:58 -0300

    Metodos do adm do sistema e bug fixes

--- src/main/java/com/SCX/ControleDeExame/infra/security/CorsConfig.java
+++ src/main/java/com/SCX/ControleDeExame/infra/security/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("https://exodus-be76.onrender.com"));
+        configuration.setAllowedOrigins(List.of("https://frontscx.onrender.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
